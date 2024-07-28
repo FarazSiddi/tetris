047022bf47cba1418ef7ac25bd7c8d8adbe49cbe
FarazSiddi <f1.siddiqui@yandex.com>
2024-07-27 20:47:28 -0400

    Added Grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 #include <raylib.h>
+#include "grid.h"
 
 int main()
 {
     Color backgroundColor = Color{20, 160, 133, 255};
 
-    const int screenWidth = 300;
-    const int screenHeight = 600;
+    const int screenWidth = 300+1;
+    const int screenHeight = 600+1;
 
     InitWindow(screenWidth, screenHeight, "Tetris");
     SetTargetFPS(60);
+
+    Grid grid = Grid();
+    grid.grid[0][0] = 1;
+    grid.grid[3][5] = 4;
+    grid.grid[17][8] = 7;
+    grid.Print();
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(backgroundColor);
+        grid.Draw();
 
         EndDrawing();
     }
