c4e81defd640e586ca5ded1bffd8b086a625474d
FarazSiddi <f1.siddiqui@yandex.com>
2024-07-28 21:49:10 -0400

    Added rotations for blocks

--- src/game.cpp
+++ src/game.cpp
@@ -48,6 +48,10 @@ void Game::HandleInput()
     case KEY_DOWN:
       MoveBlockDown();
       break;
+
+    case KEY_UP:
+      RotateBlock();
+      break;
   }
 }
 
@@ -89,4 +93,13 @@ bool Game::IsBlockOutside()
     }
   }
   return false;
+}
+
+void Game::RotateBlock()
+{
+  currentBlock.Rotate();
+  if(IsBlockOutside())
+  {
+    currentBlock.UndoRotation();
+  }
 }
