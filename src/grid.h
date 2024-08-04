7c6ebbead4ae28f5d669722a9a9126ee8d969cec
FarazSiddi <f1.siddiqui@yandex.com>
2024-08-04 00:12:48 -0400

    Added checks for completed rows and game over condition

--- src/grid.h
+++ src/grid.h
@@ -10,8 +10,12 @@ class Grid{
     void Draw();
     bool IsCellOutside(int row, int col);
     bool IsCellEmpty(int row, int col);
+    int ClearFullRows();
     int grid[20][10];
   private:
+    bool IsRowFull(int row);
+    void ClearRow(int row);
+    void MoveRowDown(int row, int numRows);
     int numRows;
     int numCols;
     int cellSize;
