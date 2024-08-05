eceb94bc3a95f2617da6792456975558dd684ff6
FarazSiddi <f1.siddiqui@yandex.com>
2024-08-04 22:56:52 -0400

    Added placeholder for level and statistics

--- src/main.cpp
+++ src/main.cpp
@@ -42,19 +42,25 @@ int main()
         BeginDrawing();
         ClearBackground(backgroundColor);
         DrawTextEx(font, "Score", {565, 15}, 38, 2, WHITE);
+        DrawTextEx(font, "Statistics", {5, 15}, 38, 2, WHITE);
         DrawTextEx(font, "Next", {570, 175}, 38, 2, WHITE);
+        DrawTextEx(font, "Level", {565, 450}, 38, 2, WHITE);
         if(game.gameOver)
         {
-            DrawTextEx(font, "Game Over", {520, 450}, 38, 2, WHITE);
+            DrawTextEx(font, "GAME OVER", {520, 565}, 38, 2, WHITE);
         }
-        DrawRectangleRounded({520, 55, 170, 60}, 0.3, 6, lightBlue);
+        DrawRectangleRounded({520, 55, 170, 60}, 0.3, 6, lightBlue); // score
 
         char scoreText[10];
         sprintf(scoreText, "%d", game.score);
         Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
 
         DrawTextEx(font, scoreText, {520 + (170 - textSize.x) / 2, 65}, 38, 2, WHITE);
-        DrawRectangleRounded({520, 215, 170, 180}, 0.3, 6, lightBlue);
+        DrawRectangleRounded({520, 215, 170, 180}, 0.3, 6, lightBlue); // next block
+
+        DrawRectangleRounded({520, 490, 170, 60}, 0.3, 6, lightBlue); // level
+
+        DrawRectangleRounded({10, 55, 170, 450}, 0.3, 6, lightBlue); // statistics
         
         game.Draw();
         EndDrawing();
